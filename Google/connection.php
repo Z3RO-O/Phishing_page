c6056f14f368b28eea0580f8bef444dd73d3c211
Prince Kumar <pk980145@gmail.com>
2023-08-15 12:25:45 +0530

    google page deleted

--- Google/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $servername="localhost";
-    $username="root";
-    $password="";
-    $db_name="fake_google";
-
-    $conn=new mysqli($servername,$username,$password,$db_name);
-    
-?>
